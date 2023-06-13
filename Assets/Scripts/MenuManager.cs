64df4d86bde653f804f6e42d3fabe547142e6094
Marvin Calvo Acuña <mar90jesus@gmail.com>
2023-06-13 14:33:01 -0600

    Working on the world map

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuManager : MonoBehaviour
+{
+    public void  ShowScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
